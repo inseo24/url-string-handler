30d97e38a4deb2907324b9d3f4dbef3085c6df4f
choi seoin <inseo@uupfi.com>
2022-03-18 14:10:41 +0900

    setting: 컨트롤러, 서비스, dto

---
+++ src/main/java/com/handler/stringhandler/controller/ParseController.java
@@ -0,0 +1,29 @@
+package com.handler.stringhandler.controller;
+
+import com.handler.stringhandler.dto.ParseRequest;
+import com.handler.stringhandler.dto.ParseResponse;
+import com.handler.stringhandler.service.ParseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class ParseController {
+
+    private final ParseService parseService;
+
+    /*
+     request -> url, type, 출력 묶음 단위
+     response -> 몫, 나머지
+     */
+    @PostMapping("/ api/parse")
+    public ResponseEntity<ParseResponse> parse(@RequestBody @Valid ParseRequest request) {
+        final ParseResponse response = parseService.parse(request);
+        return null;
+    }
+}
