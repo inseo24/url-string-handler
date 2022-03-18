fa942c5f4843ad5ac5205a0faf5feaf3d4fda9e2
choi seoin <inseo@uupfi.com>
2022-03-18 14:42:43 +0900

    feat: UrlConnector 추가

--- src/main/java/com/handler/stringhandler/parse/service/ParseService.java
+++ src/main/java/com/handler/stringhandler/parse/service/ParseService.java
@@ -2,12 +2,18 @@ package com.handler.stringhandler.parse.service;
 
 import com.handler.stringhandler.parse.dto.ParseRequest;
 import com.handler.stringhandler.parse.dto.ParseResponse;
+import com.handler.stringhandler.parse.util.UrlConnector;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ParseService {
 
+    private final UrlConnector urlConnector;
+
     public ParseResponse parse(ParseRequest request) {
+        urlConnector.getHtml(request.getUrl());
         return null;
     }
 }
---
+++ src/test/java/com/handler/stringhandler/parse/util/UrlConnectorTest.java
@@ -0,0 +1,32 @@
+package com.handler.stringhandler.parse.util;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class UrlConnectorTest {
+
+    private final UrlConnector urlConnector = new UrlConnector();
+
+
+    @DisplayName("잘못된 url 입력시 IllegalArgumentException 발생")
+    @Test
+    void urlFailTest() {
+        assertThatThrownBy(() -> urlConnector.getHtml("wrong.url.ccc"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("url 입력 시 html 을 가져올 수 있다.")
+    @ParameterizedTest
+    @CsvSource({"https://www.naver.com, <title>NAVER</title>", "https://www.google.com, <title>Google</title>"})
+    void urlSuccessfulTest(final String url, final String title) {
+        final String result = urlConnector.getHtml(url);
+
+        assertThat(result.contains(title)).isTrue();
+    }
+}
