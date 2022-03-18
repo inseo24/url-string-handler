d41be93a521299524182806ed9f6ff2d6ed70a72
choi seoin <inseo@uupfi.com>
2022-03-18 15:00:31 +0900

    feat: 노출 유형 추가

--- src/main/java/com/handler/stringhandler/parse/util/UrlConnector.java
+++ src/main/java/com/handler/stringhandler/parse/util/UrlConnector.java
@@ -9,9 +9,16 @@ import java.io.IOException;
 public class UrlConnector {
 
     public String getHtml(final String url){
-        // jsoup
         try {
             return Jsoup.connect(url).get().html();
+        } catch (IOException e){
+            throw new IllegalArgumentException("접근할 수 없는 url 입니다.");
+        }
+    }
+
+    public String getText(final String url){
+        try {
+            return Jsoup.connect(url).get().text();
         } catch (IOException e){
             throw new IllegalArgumentException("접근할 수 없는 url 입니다.");
         }
